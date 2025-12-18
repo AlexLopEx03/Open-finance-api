344ba5bf04355cefd209dc4a21138fe3e8d68ba0
AlexLopEx03 <164388886+AlexLopEx03@users.noreply.github.com>
2025-12-18 18:58:22 +0000

    Cors edit

--- open-finance-api/app/settings.py
+++ open-finance-api/app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1:5500']
 
 
 # Application definition
